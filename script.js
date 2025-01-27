bba100dfd8165c670bddd7cc4b3a4c2e5c3af176
Lawrence Miracle <87551676+lawrencemiracle@users.noreply.github.com>
2025-01-27 20:46:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ var span =  document.getElementById("span");
+
+ span.style.color = "#39b54a";
+
+ function span(){
+     if (span.style.opacity = "#39b54a") 
+     {
+        span.style.opacity = "red";
+     } 
+     else {
+        span.style.opacity = "#39b54a";
+     }
+ }
